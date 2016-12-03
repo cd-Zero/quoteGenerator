cea853bfc962f479361a5c52d8c68ae016a8be3b
Michael Aduayi <Mike.Aduayi@gmail.com>
2016-12-02 23:56:34 -0500

    Handlebars.js is used for injecting data in view/ twitter share button works and populates text with quote

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-var button =  document.querySelector("#btn");
-var q      =  document.querySelector('#quote')
+var button            =  document.querySelector("#btn");
+var q                 =  document.querySelector('#quote');
+var tweetbutton       =  document.querySelector("#tweet");
+var blockquote        =  document.querySelector('blockquote');
+
+
+
+
+
+
+
+
 // button initiates XMLHttpRequest for data
-button.addEventListener("click", function(){
-var random = Math.floor((Math.random() * 10) + 1)
+button.addEventListener("click", function(e){
+var random = Math.floor((Math.random() * 25) + 1)
 var xhr    =  new XMLHttpRequest();
 
 xhr.open('GET',"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]="+random,true);
   xhr.onload    = function(){
   var quoteData = JSON.parse(xhr.responseText);
 
-    console.log(quoteData[0]);
-
     changetext(quoteData);
-
-
 
 //  renderHTML(quoteData);
 }
@@ -23,17 +29,22 @@ xhr.open('GET',"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&fil
 
 
 
+
+//creates button
   function changetext(data){
-  q.innerHTML = data[0].content;
-  }
+  //q.innerHTML   = data[0].content;
+  console.log("testing");
+  console.log(data);
+
+   var rawTemplate                = document.querySelector('#quoteTemplate').innerHTML;
+   var compileTemplate            = Handlebars.compile(rawTemplate);
+   var generatedHTML              = compileTemplate(data[0]);
+   var quoteContainer             = document.querySelector('#quote');
+   quoteContainer.innerHTML       = generatedHTML;
+
+  var inject = document.querySelector('#quote').innerText;
+
+   document.querySelector("#share").href = 'https://twitter.com/intent/tweet?text=' +inject;
 
 
-
-
-
-
-//id = butt, button
-// id = quote, quoteblock
-
-
-//console.log(q);
+}
