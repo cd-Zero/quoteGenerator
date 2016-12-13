7f0bdb2ee0e2ce18bf836bbacd8e897a1130e623
Michael Aduayi <Mike.Aduayi@gmail.com>
2016-12-13 13:28:46 -0500

    added browser sync functionality... auto refresh when html and css files changed

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@
       autoprefixer  = require('autoprefixer'),
       cssvars       = require('postcss-simple-vars'),
       nested        = require('postcss-nested'),
-      cssImport     = require('postcss-import');
+      cssImport     = require('postcss-import'),
+      browserSync   = require('browser-sync').create();
+
 // test gulp function
 gulp.task('default', function(){
     console.log("gulp function correctly!");
@@ -18,19 +20,33 @@ return  gulp.src('/home/zero/Desktop/fccProjects/quote-generator/assets/css/main
 });
 
 gulp.task('html', function(){
-  console.log("html files linked");
+  // console.log("html files linked");
+  // refresh browserSync when html changes
+  browserSync.reload();
 });
 
 // gulp-watch automation
 
 gulp.task("watch",function(){
+
+  browserSync.init({
+    server:{
+      baseDir:"/home/zero/Desktop/fccProjects/quote-generator"
+    }
+  })
+
   watch('/home/zero/Desktop/fccProjects/quote-generator/index.html',function(){
     // detects html changes
     gulp.start('html')
   })
 
   watch("/home/zero/Desktop/fccProjects/quote-generator/assets/css/**/*.css",function(){
-    gulp.start('css')
+    gulp.start('cssInject')
   })
 
+});
+
+// have browserSync refresh when css is changed
+gulp.task('cssInject',[/*dependencies completed first*/'css'],function(){
+  gulp.src("/home/zero/Desktop/fccProjects/quote-generator/temp/main.css").pipe(browserSync.stream());
 });
